cb54f64c9d5d535fd07d983917058d507303d880
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2023-04-25 23:28:14 +0300

    BoostPlatform-luokan testit lisätty

---
+++ src/tests/boost_platform_test.py
@@ -0,0 +1,17 @@
+import unittest
+from sprites.boost_platform import BoostPlatform
+
+
+class TestBoostPlatform(unittest.TestCase):
+    def setUp(self):
+        self.platform = BoostPlatform(100,100,64)
+
+    def test_platform_moves_down(self):
+        self.platform.rect.y = 200
+        self.platform.update(5)
+        self.assertNotEqual(self.platform.rect.y, 200)
+
+    def test_platform_is_deleted_after_it_leaves_screen(self):
+        self.platform.rect.y = 605
+        self.platform.update(0)
+        self.assertTrue(self.platform, False)
