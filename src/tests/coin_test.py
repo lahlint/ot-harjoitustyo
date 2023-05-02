fc61daef7428934b94fa4c2d6a622c25538d1a8a
Lahja Lintumäki <lahja.lintumaki@gmail.com>
2023-05-02 19:39:39 +0300

    coin-luokan testiluokka ja testit lisätty

---
+++ src/tests/coin_test.py
@@ -0,0 +1,16 @@
+import unittest
+from sprites.coin import Coin
+from game import Game
+
+class TestCoin(unittest.TestCase):
+    def setUp(self):
+        self.coin = Coin(200,200)
+
+    def test_coin_moves_down(self):
+        self.coin.update(5)
+        self.assertNotEqual(self.coin.rect.y, 200)
+    
+    def test_coin_is_deleted_after_it_leaves_screen(self):
+        self.coin.rect.y = 605
+        self.coin.update(0)
+        self.assertTrue(self.coin, False)
